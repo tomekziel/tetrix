b8636cb3a25fc5aa0ca23b4060adf90c5895b313
Tomasz Zielinski <tomasz.zielinski@gmail.com>
2023-08-08 20:31:29 +0200

    Rysowanie klocka, przełączanie między klockami

--- Tetrix.cs
+++ Tetrix.cs
@@ -14,6 +14,35 @@ namespace Tetrix
 
         int[,] well = new int[wellHeight, wellWidth];
 
+        readonly static List<int [,]> pieces = new List<int[,]> {
+            new int[,] 
+                {
+                    {0,0,0,0},
+                    {1,1,1,1},
+                    {0,0,0,0},
+                    {0,0,0,0}
+                }
+            ,
+            new int[,]
+                {
+                    {1,0,0},
+                    {1,1,1},
+                    {0,0,0}
+                }
+            ,
+            new int[,]
+                {
+                    {0,0,1},
+                    {1,1,1},
+                    {0,0,0}
+                }
+
+        };
+
+        int pieceNo = 0;
+        int pieceX = 5;
+        int pieceY = 20;
+
         static void Main(string[] args)
         {
             new Tetrix().MainLoop();
@@ -23,8 +52,16 @@ namespace Tetrix
         {
             Init();
             Console.WriteLine("Tetrix!");
-            DrawScreen();
-            Console.ReadLine();
+            while (true) { 
+                DrawScreen();
+                DrawPiece();
+                Console.ReadKey();
+                pieceNo++;
+                if (pieceNo == pieces.Count)
+                {
+                    pieceNo = 0;
+                }
+            }
 
         }
 
@@ -33,11 +70,14 @@ namespace Tetrix
             well[0, 1] = well[0, 2] = well[0, 3] = well[0, 4] = well[1, 4] = well[1, 5] = 1;
         }
 
+        int wellMariginX = 10;
+        int wellMariginY = 22;
+
         public void DrawScreen()
         {
             for (int y = 0; y < wellHeight; y++)
             {
-                Console.SetCursorPosition(10, 22 - y);
+                Console.SetCursorPosition(wellMariginX, wellMariginY - y);
                 Console.Write("##");
                 for (int x=0; x<wellWidth; x++)
                 {
@@ -45,9 +85,25 @@ namespace Tetrix
                 }
                 Console.Write("## "+y);
             }
-            Console.SetCursorPosition(10, 23);
+            Console.SetCursorPosition(wellMariginX, wellMariginY+1);
             Console.Write("########################");
+        }
 
+        public void DrawPiece()
+        {
+            var currentPiece = pieces[pieceNo];
+            var dim = currentPiece.GetLength(0); // always square
+            for (int y = 0; y < dim; y++)
+            {
+                for (int x = 0; x < dim; x++)
+                {
+                    int drawX = wellMariginX + pieceX*2 + x*2;
+                    int drawY = wellMariginY - pieceY + y;
+                    Console.SetCursorPosition(drawX, drawY);
+                    Console.Write(currentPiece[y, x] == 0 ? "  " : "()");
+                }
+
+            }
 
         }
 
